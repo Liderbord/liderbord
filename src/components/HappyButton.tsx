b5b434e082de533e4f5ebd852e6bee7c38c864ce
Matthieu Roux <36091631+M4TTRX@users.noreply.github.com>
2022-04-15 09:53:30 +0200

    fixed some button values

--- src/components/HappyButton.tsx
+++ src/components/HappyButton.tsx
@@ -2,39 +2,10 @@ import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 
 const HappyButton = styled(Button)({
-    boxShadow: '0px 0px 10px 1px',
-    textTransform: 'none',
-    fontSize: 20,
-    padding: '6px 12px',
-    border: '1px solid',
-    lineHeight: 1.5,
-    borderRadius: '10px',
-    fontFamily: [
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:hover': {
-      backgroundColor: '#FF7800',
-    },
-    '&:active': {
-      backgroundColor: '#FF6700',
-    },
-    palette: {
-        primary: {
-          main: '#FF7900',
-        },
-        secondary: {
-          main: '#E3E3E3',
-          contrastText: '#fff',
-        },
-      },
+  boxShadow: "0px 0px 10px 1px rgba(0, 0, 0, 0.1)",
+  textTransform: "none",
+  padding: "12px 24px",
+  height: "49px",
 });
 
 export default HappyButton;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import "../styles/login.css";
 import { useMoralis } from "react-moralis";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { theme } from "../styles/theme";
