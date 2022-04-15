0af38825d2d08c6ff001b414813c24033735ebbb
Matthieu Roux <36091631+M4TTRX@users.noreply.github.com>
2022-04-15 10:29:14 +0200

    Restored main login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "../styles/login.css";
 import { useMoralis } from "react-moralis";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../styles/theme";
 import { useNavigate } from "react-router-dom";
+import Moralis from "moralis";
 import HappyButton from "../components/HappyButton";
-import Moralis from 'moralis'
 
 function Login() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -57,70 +52,17 @@ function Login() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <img src={require("../res/logo.png")} alt="failure" />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-            </Grid>
-            <div>
-              <HappyButton
-                color="secondary"
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Register
-              </HappyButton>
-              <HappyButton
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </HappyButton>
-            </div>
-          </Box>
-        </Box>
-      </Container>
+      <Box id="loginform">
+        <HappyButton title="Log in" onClick={login}>
+          Login{" "}
+        </HappyButton>
+        <HappyButton title="Register" onClick={goToRegisterPage}>
+          Register
+        </HappyButton>
+        <HappyButton onClick={logOut} disabled={isAuthenticating}>
+          Logout
+        </HappyButton>
+      </Box>
     </ThemeProvider>
   );
 }
