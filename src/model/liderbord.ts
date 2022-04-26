cb75e4bd8c6dfc2ad588074b866b90c2fc8a31df
Matthieu Roux <36091631+M4TTRX@users.noreply.github.com>
2022-04-26 16:20:09 +0200

    Added optional liderbord values

--- src/model/liderbord.ts
+++ src/model/liderbord.ts
@@ -4,6 +4,8 @@ interface Liderbord {
   id: string;
   title: string;
   tags: string[];
+  nbVotes?: number; // number of votes on all resources in the liderbord
+  nbResources?: number; // number of resources in the liderbord
   resources: Resource[];
 }
 
