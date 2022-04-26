bc5cd20a45ed5462adf02a9d3d2d3f26ecce0031
Matthieu Roux <36091631+M4TTRX@users.noreply.github.com>
2022-04-26 16:26:34 +0200

    Added a description field, renamed title to topic

--- src/model/liderbord.ts
+++ src/model/liderbord.ts
@@ -2,7 +2,8 @@ import Resource from "./resource";
 interface Liderbord {
   // Identifier of the liderbord (key)
   id: string;
-  title: string;
+  topic: string;
+  description: string;
   tags: string[];
   nbVotes?: number; // number of votes on all resources in the liderbord
   nbResources?: number; // number of resources in the liderbord
