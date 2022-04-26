1cf4bcaf167802f4581596d73193b8f00534edc6
Matthieu Roux <36091631+M4TTRX@users.noreply.github.com>
2022-04-26 15:40:24 +0200

    Navigation & login rework (#12)
    [fix]: moved the moralis in a secrets file
    [chore]: changed navigation naming
    [fix]: made the login work better
    [fix]: fixed CodeQL issues
    [fix]: tag shifting in liderbord creation
    
    Co-authored-by: Rahim Belateche <rayanebelateche@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { MoralisProvider } from "react-moralis";
+import moralisKeys from "./moralis-keys.json";
+import { theme } from "./styles/theme";
+import { ThemeProvider } from "@emotion/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider serverUrl="https://ioncbc419bjd.usemoralis.com:2053/server" appId="qqNoQcpJSGcE9m7Oov43EcpSF64OrZ40PiZyLVzt">
-      <App />
+    <MoralisProvider
+      serverUrl={moralisKeys.serverUrl}
+      appId={moralisKeys.appId}
+    >
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </MoralisProvider>
   </React.StrictMode>,
   document.getElementById("root")
