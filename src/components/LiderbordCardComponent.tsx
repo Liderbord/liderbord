1b925e617c85617dca3215e63326f144170f91fb
Rahim Belateche <rayanebelateche@gmail.com>
2022-04-28 17:52:01 +0100

    add filter item component

--- src/components/LiderbordCardComponent.tsx
+++ src/components/LiderbordCardComponent.tsx
@@ -1,69 +1,59 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Liderbord from '../model/liderbord';
-
-
-/**
- * 
- * @param props 
- * title
- * tags
- * nbVotes
- * nbResources
- * topic
- */
-
-
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Liderbord from "../model/liderbord";
 
 const bull = (
-    <Box
-      component="span"
-      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-      •
-    </Box>
-  );
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
 
-
-function LiderbordCard({liderbord} : {liderbord : Liderbord}) {
-    return(
-  <React.Fragment>
-      <Card variant="outlined" sx={{
-          height: 140,
-          width: 900,
+function LiderbordCard({ liderbord }: { liderbord: Liderbord }) {
+  return (
+    <React.Fragment>
+      <Card
+        variant="outlined"
+        sx={{
+          height: 150,
+          width: 1000,
           borderRadius: "24px",
           pb: "24px",
           mt: "16px",
-           }}>
-    <CardContent>
-      <Typography sx={{mb : 2}} variant="h2" component="div">
-        {liderbord.topic}
-      </Typography>
-      <hr style={{
-          
-          width: "50%",
-          border: "1px solid blue",
-          margin: "5px"
-      }}></hr>
-      <Typography sx={{ display:"inline"}} color="text.secondary" >
-          {liderbord.tags.map((item,i) => <span key={i}> #{item} </span>)}
-          {bull} {liderbord.nbVotes} {bull} {liderbord.nbResources}
-      </Typography>
-      <Typography sx={{mt: 1.5}}variant="body2">
-        {liderbord.description}
-        
-      </Typography>
-    </CardContent>
-    </Card>
-  </React.Fragment>
-
-    )
+        }}
+      >
+        <CardContent>
+          <Typography sx={{ mb: 1 }} variant="h2" component="div">
+            {liderbord.topic.toUpperCase()}
+          </Typography>
+          <hr
+            style={{
+              width: "50%",
+              border: "0.9px solid blue",
+              margin: "7px",
+            }}
+          ></hr>
+          <Typography sx={{ display: "inline" }} color="text.secondary">
+            {liderbord.tags.map((item, i) => (
+              <b key={i}> #{item} </b>
+            ))}
+            {bull} {liderbord.nbVotes} {bull} {liderbord.nbResources}
+          </Typography>
+          <Typography sx={{ mt: 1.5 }} variant="body2">
+            {liderbord.description}
+          </Typography>
+        </CardContent>
+      </Card>
+    </React.Fragment>
+  );
 }
 
 export default LiderbordCard;
--- src/pages/searchLiderbord.tsx
+++ src/pages/searchLiderbord.tsx
@@ -1,57 +1,88 @@
 import React from "react";
 import LiderbordCard from "../components/LiderbordCardComponent";
+import HappyButton from "../components/HappyButton";
 import Liderbord from "../model/liderbord";
-import { Typography, Stack } from "@mui/material";
+import { Typography, Stack, Container, Grid, Box } from "@mui/material";
+import FilterItem from "../components/FilterItem";
 import { useNavigate, Navigate } from "react-router-dom";
 
-
-
 export default function SearchLiderbord(props: any) {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  // temporary data for view
 
-    // temporary data for view 
+  const liderbord1: Liderbord = {
+    id: "111",
+    topic: "ADVANCED C++",
+    description:
+      "High level C++ courses, these courses should focus only on the more advanced concepts of teh c++ langauge. Basic c++ introductions are not welcome",
+    tags: ["programming", "advanced", "C++"],
+    nbVotes: 2,
+    nbResources: 3,
+    resources: [],
+  };
 
-    const liderbord1 : Liderbord = {
-        id : "111",
-        topic : "ADVANCED C++",
-        description : "High level C++ courses, these courses should focus only on the more advanced concepts of teh c++ langauge. Basic c++ introductions are not welcome",
-        tags : ["programming", "advanced", "C++"],
-        nbVotes : 2,
-        nbResources : 3 ,
-        resources : []   
-    }
+  const liderbord2: Liderbord = {
+    id: "112",
+    topic: "BEGINNER C++",
+    description:
+      "Best introduction to C++, for people that have never programmed",
+    tags: ["programming", "beginner", "C++"],
+    nbVotes: 400,
+    nbResources: 25,
+    resources: [],
+  };
 
-    const liderbord2 : Liderbord = {
-        id : "112",
-        topic : "BEGINNER C++",
-        description : "Best introduction to C++, for people that have never programmed",
-        tags : ["programming", "beginner", "C++"],
-        nbVotes : 400,
-        nbResources : 25,
-        resources : [] 
-    }
+  const liderbord3: Liderbord = {
+    id: "113",
+    topic: "TESTING IN C++",
+    description: "Best testing frameworks on C++, best way to test your code",
+    tags: ["programming", "advanced", "C++", "testing"],
+    nbVotes: 300,
+    nbResources: 25,
+    resources: [],
+  };
 
+  const liderbords = [liderbord1, liderbord2, liderbord3];
 
-    const liderbord3 : Liderbord = {
-        id : "113",
-        topic : "TESTING IN C++",
-        description : "Best testing frameworks on C++, best way to test your code",
-        tags : ["programming", "advanced", "C++", "testing"],
-        nbVotes : 300,
-        nbResources : 25,
-        resources : [] 
-    }   
+  return (
+    <Container>
+      <Grid container spacing={7} columns={16} sx={{ mt: "16px" }}>
+        <Grid item xs={12}>
+          <Typography variant="h6">
+              FILTERS
+          </Typography>
+            
+          
 
-    const liderbords = [liderbord1, liderbord2, liderbord3];
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={5} columns={16}>
+              <Grid item xs={4}>
+                <FilterItem>C++</FilterItem>
+              </Grid>
+              <Grid item xs={4}>
+                <FilterItem>Beginner</FilterItem>
+              </Grid>
+              <Grid item xs={4}>
+                <FilterItem>Advanced</FilterItem>
+              </Grid>
+            </Grid>
+          </Box>
 
-    return(
-        <Stack spacing={2} sx={{ marginTop: "100px" }}>
-            {liderbords.map((liderbord, index) => 
-                <LiderbordCard key={index} liderbord={liderbord} />
-            )}
-        
-        </Stack>
-        
-    );
+        </Grid>
+
+        <Grid item xs={4} >
+          <HappyButton color="secondary" variant="contained">
+            + Create a Liderbord
+          </HappyButton>
+        </Grid>
+      </Grid>
+
+      <Stack spacing={2} sx={{ marginTop: "20px" }} alignItems="center">
+        {liderbords.map((liderbord, index) => (
+          <LiderbordCard key={index} liderbord={liderbord} />
+        ))}
+      </Stack>
+    </Container>
+  );
 }
