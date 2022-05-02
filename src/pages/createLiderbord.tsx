1cc77932e942f07c1d982111ff832cd25529fdd7
Ewen Chaillan <echaillan@insa-lyon.fr>
2022-05-02 09:38:15 +0200

    supp model liderbord

--- src/pages/createLiderbord.tsx
+++ src/pages/createLiderbord.tsx
@@ -6,7 +6,6 @@ import { FormControl, tooltipClasses } from "@mui/material";
 import { useNavigate, Navigate } from "react-router-dom";
 import Moralis from "moralis";
 import { useState } from "react";
-import { Liderbord } from "../back_end/models/liderbord";
 import { Grid, Typography } from "@mui/material";
 import HappyButton from "../components/HappyButton";
 import HappyTextField from "../components/HappyTextField";
@@ -68,8 +67,8 @@ function CreateLiderbord() {
 
     // if there are no errors proceed with the submission of the liderbord
     if (topicError + descriptionError + tagError === "") {
-      const liderbord = new Liderbord(topic, description);
-      liderbord.createLiderbord();
+      await Moralis.Cloud.run("createLiderbord", params);
+      
     }
   };
 
