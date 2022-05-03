9f6e229d4d6234b715fa352e3c9a0351a5234e75
Alexis <alexismrx@hotmail.fr>
2022-05-03 10:40:25 +0200

    Add the login/logout feature

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,11 +3,20 @@ import { logDOM } from '@testing-library/react';
 import { useState } from 'react';
 import liderbordLogo from "../res/tinyLogo.png";
 import { useMoralis } from "react-moralis";
+import { useNavigate, Navigate } from "react-router-dom";
+import userIcon from "../res/icons/authent.png";
+import notAuthentIcon from "../res/icons/notAuthent.png";
 
 
 export default function NavigationBar() {
     let { isAuthenticated, logout, user } = useMoralis();
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const navigate = useNavigate();
+
+    function redirectLogin() {
+        navigate('/login');
+    }
+   
 
     const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
@@ -21,8 +30,6 @@ export default function NavigationBar() {
         <AppBar position="absolute" style={{ background: 'transparent', boxShadow: 'none'}}>
             <Container maxWidth="xl">
                 <Toolbar >
-                    
-                    
                     <Box sx={{ flexGrow: 1, mt: 3, display: { xs: 'none', md: 'flex' } }}>
                         <div className="container">
                             <img height={30} width={200} src={liderbordLogo} alt="liderbord Logo" />
@@ -39,7 +46,7 @@ export default function NavigationBar() {
                             onClick={handleMenu}
                             color="inherit"
                         >
-                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            <img width={40} height={40} src={userIcon} alt="user" />
                         </IconButton>
                         <Menu
                             id="menu-appbar"
@@ -59,6 +66,37 @@ export default function NavigationBar() {
                             <MenuItem onClick={handleClose}>Profile</MenuItem>
                             <MenuItem onClick={handleClose}>My account</MenuItem>
                             <MenuItem onClick={logout}>Log out</MenuItem>
+                        </Menu>
+                        </div>
+                    )}
+                    {!isAuthenticated && (
+                        <div>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleMenu}
+                            color="inherit"
+                        >
+                            <img width={40} height={40} src={notAuthentIcon} alt="Not Authent" />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                            }}
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={redirectLogin}>Log in</MenuItem>
                         </Menu>
                         </div>
                     )}
