e0f9bc879e2f890449477a68f9aa86de0e55f99c
patriciobcs <38959764+patriciobcs@users.noreply.github.com>
2022-11-24 11:49:43 +0100

    Web3 (#32)
    * feat(web3): add resource and vote
    
    * feat(web3): claim HC and merge votes
    
    * feat: change smart contract
    
    * add liderbord already created handling
    
    * Made icons in comments stay the same size with longer comments
    
    * feat: add user data and HC
    
    * feat: notifications
    
    * fix: useLiderbordContract propagation
    
    * feat: loading bar
    
    Co-authored-by: Rahim Belateche <rayanebelateche@gmail.com>
    Co-authored-by: Matthieu Roux <36091631+M4TTRX@users.noreply.github.com>

---
+++ src/context/BiconomyProvider.js
@@ -0,0 +1,89 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { createContext, useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
+import { Biconomy } from "@biconomy/mexa";
+import Web3 from "web3";
+import { notification } from "antd";
+import { networkConfigs } from "helpers/networks";
+import liderbordsContract from "contracts/Liderbords.json";
+import biconomyApiKey from "helpers/biconomy";
+
+export const BiconomyContext = createContext({});
+
+const BiconomyContextProvider = (props) => {
+  const { children } = props;
+  const {
+    isWeb3Enabled,
+    isAuthenticated,
+    isWeb3EnableLoading,
+    Moralis,
+    enableWeb3,
+  } = useMoralis();
+  const [isBiconomyInitialized, setIsBiconomyInitialized] = useState(false);
+  const [isBiconomyInitializing, setIsBiconomyInitializing] = useState(false);
+  const [biconomyProvider, setBiconomyProvider] = useState({});
+  const [contract, setContract] = useState({});
+  const { abi } = liderbordsContract;
+  const contractAddress = process.env.REACT_APP_LIDERBORD_ADDRESS;
+
+  useEffect(() => {
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) {
+      enableWeb3();
+    }
+  }, [isAuthenticated, isWeb3Enabled]);
+
+  useEffect(() => {
+    const initializeBiconomy = async () => {
+      setIsBiconomyInitializing(true);
+      if (isBiconomyInitialized) {
+        // Resetting when reinitializing
+        setIsBiconomyInitialized(false);
+      }
+      await Moralis.enableWeb3();
+      const web3 = new Web3(Moralis.provider);
+      const networkProvider = new Web3.providers.HttpProvider(
+        networkConfigs["0x13881"]?.rpcUrl
+      );
+
+      const biconomy = new Biconomy(networkProvider, {
+        walletProvider: web3.currentProvider,
+        apiKey: biconomyApiKey["0x13881"],
+        debug: true,
+      });
+      setBiconomyProvider(biconomy);
+
+      // This web3 instance is used to read normally and write to contract via meta transactions.
+      web3.setProvider(biconomy);
+
+      biconomy
+        .onEvent(biconomy.READY, () => {
+          setIsBiconomyInitialized(true);
+          setIsBiconomyInitializing(false);
+          const contractInst = new web3.eth.Contract(abi, contractAddress);
+          setContract(contractInst);
+        })
+        .onEvent(biconomy.ERROR, () => {
+          // Handle error while initializing mexa
+          notification.error({
+            message: "Biconomy Initialization Fail",
+            description:
+              "Biconomy has failed to initialized. Please try again later.",
+            placement: "bottomLeft",
+          });
+        });
+    };
+    if (isAuthenticated && isWeb3Enabled && !isBiconomyInitializing) {
+      initializeBiconomy();
+    }
+  }, [isAuthenticated, isWeb3Enabled, isWeb3EnableLoading]);
+
+  return (
+    <BiconomyContext.Provider
+      value={{ isBiconomyInitialized, biconomyProvider, contract }}
+    >
+      {children}
+    </BiconomyContext.Provider>
+  );
+};
+
+export default BiconomyContextProvider;
